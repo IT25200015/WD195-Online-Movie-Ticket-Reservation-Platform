c6e21b3cca44e1e8226ad91abe0358d043fd2b74
IT25200015 <it25200015@my.sliit.lk>
2026-03-02 09:40:58 +0530

    feat : create abstract User class for User management

--- OnlineMovieTicketBooking/src/main/java/com/cinebooking/util/DBConnection.java
+++ OnlineMovieTicketBooking/src/main/java/com/cinebooking/util/DBConnection.java
@@ -5,9 +5,9 @@ import java.sql.SQLException;
 
 public class DBConnection {
 
-    private static final String URL = "jdbc:mysql://localhost:3307/cinebooking_db";
+    private static final String URL = "jdbc:mysql://localhost:3306/cinebooking_db";
     private static final String USER = "root";
-    private static final String PASSWORD = "";
+    private static final String PASSWORD = "1234";
 
     public static Connection getConnection() throws SQLException, ClassNotFoundException {
         Class.forName("com.mysql.cj.jdbc.Driver");
