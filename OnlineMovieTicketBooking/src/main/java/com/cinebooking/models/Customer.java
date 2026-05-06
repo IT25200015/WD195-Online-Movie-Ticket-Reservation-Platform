e9433b60e97316114ef5d560047880934630c116
IT25100239 <IT25100239@my.sliit.lk>
2026-05-06 02:35:23 -0800

    promotion and discount model classes added

--- OnlineMovieTicketBooking/src/main/java/com/cinebooking/models/Customer.java
+++ OnlineMovieTicketBooking/src/main/java/com/cinebooking/models/Customer.java
@@ -2,27 +2,14 @@ package com.cinebooking.models;
 
 public class Customer extends User {
 
-    private String membership;
-
     public Customer() {
         super();
         this.setRole("Customer");
-        this.membership = "Regular";
     }
     //Parameterized constructor to create a Customer object
-    public Customer(String name, String email, String password, String mobileNumber, String dob, String gender, String membership) {
-        super(name, email, password, "Customer", mobileNumber, dob, gender);
-        this.membership = membership;
+    public Customer(int userId ,String name ,String email, String password) {
+        super(userId, name, email, password, "Customer");
     }
-
-    public String getMembership() {
-        return membership;
-    }
-
-    public void setMembership(String membership) {
-        this.membership = membership;
-    }
-
     @Override
     public void displayDashboard() {
         System.out.println("Welcome to the Customer Dashboard");
